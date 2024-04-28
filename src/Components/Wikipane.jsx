8cc2b8dbbd5efa8df4703c954c70c8a3b5c0bd83
VerianCS <zafkiel023@gmail.com>
2024-04-28 22:01:09 +0200

    Added stilysh to the side left pane, added drop menu cpaability to evert single entry on the wiki with every element.

--- src/Components/Wikipane.jsx
+++ src/Components/Wikipane.jsx
@@ -1,23 +1,28 @@
 
     const addRows = Array.from({length: 20}, (_, i) => i+1);
-    
+        
 
 function Wikipane() 
 {
     return (
         <>
             <div className="wikiPane overflow-y-auto p-4 flex flex-col max-h-screen">
-                    <div className="wikiPanelTitle max-h-48">
+                    <div className="wikiPanelTitle max-h-48 rounded-md shadow-md backdrop-blur-sm">
                         <h1 className="text-red-700">Crimson Megu Bot</h1>
                         <h2 className=" text-center text-slate-700">Codebase</h2>
                         <br></br>
                     </div>
-                <ul className="wikiList text-slate-500 p-5">
-                    {addRows.map((item) => (
-                        
-                        <li key={item}>Item {item}</li>
+            <div className="ElementsDiv p-2 flex flex-col ">
+                <ul className="wikiList text-slate-500 py-2 ">
+                       {addRows.map((item) => (
+                            <div key={item} className="itemWikiType outline outline-1 outline-red-950 m-2 p-2 rounded-md flex flex-row">
+                                <li key={item}>Item {item}</li>
+                            
+                            </div>
                     ))}
                 </ul>
+            </div>
+
             </div>
         </>
 
